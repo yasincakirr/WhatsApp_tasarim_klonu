264e8cb8c7281796d8f56ac0b94119b13d76bf70
yasincakirr <104265358+yasincakirr@users.noreply.github.com>
2024-03-26 00:37:34 +0300

    baslangic

---
+++ app/src/main/java/com/yasincakir/whatsapp_tasarm_klon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yasincakir.whatsapp_tasarm_klon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
