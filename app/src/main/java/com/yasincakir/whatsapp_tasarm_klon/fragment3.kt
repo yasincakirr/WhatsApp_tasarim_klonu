5baa304ddbbf8a9faab87e2ef32d57ffb291faaa
yasincakirr <104265358+yasincakirr@users.noreply.github.com>
2024-04-10 16:36:55 +0300

    durum ve arama tasarimi

--- app/src/main/java/com/yasincakir/whatsapp_tasarm_klon/fragment3.kt
+++ app/src/main/java/com/yasincakir/whatsapp_tasarm_klon/fragment3.kt
@@ -1,16 +1,34 @@
 package com.yasincakir.whatsapp_tasarm_klon
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.yasincakir.whatsapp_tasarm_klon.databinding.FragmentFragment3Binding
 
 class fragment3 : Fragment() {
 
+    private lateinit var binding:FragmentFragment3Binding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment3, container, false)
+
+        binding = FragmentFragment3Binding.inflate(inflater, container, false)
+
+
+
+        binding.floatingActionButton2.setOnClickListener {
+
+            var intent = Intent(activity,MainActivity2::class.java)
+
+            startActivity(intent)
+
+        }
+
+
+
+        return binding.root
     }
 
 
